292b2e0cf3e6d0543f94675ea1b7ed633414d9bb
Richard Jácomo <richard.jacomo23@gmail.com>
2023-01-04 18:50:36 -0300

    feat: static dashboard creation

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React from "react";
+import { AppRoutes } from "./routes/routes";
 
 function App() {
   return (
-    <div>Teste</div>
+    <div className="App">
+      <AppRoutes />
+    </div>
   );
 }
 
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,38 @@
-import React from 'react'
+import React from "react";
+import { ButtonLogout, ButtonMenu } from "../../styles/Buttons/styled";
+import { FigureBackgroundDashboard } from "../../styles/Figures/styles";
+import { Main } from "./style";
 
 export const DashboardPage = () => {
   return (
-    <div>DashboardPage</div>
-  )
-}
+    <>
+      <FigureBackgroundDashboard>
+        <img src="/bg-dashboard.png" alt="" />
+      </FigureBackgroundDashboard>
+      <Main>
+        <section className="section-dashboard">
+          <div className="div-menu">
+            <div className="div-logo-and-buttons">
+              <figure className="logo">
+                <img src="/logo.svg" alt="" />
+              </figure>
+              <ButtonMenu className="first-button-menu">
+                Criar torneio
+              </ButtonMenu>
+              <ButtonMenu className="secound-button-menu">
+                Meus torneios
+              </ButtonMenu>
+              <ButtonMenu className="third-button-menu">Meu time</ButtonMenu>
+            </div>
+            <div className="div-button-logout">
+              <ButtonLogout className="button-logout">
+                <img className="icon-logout" src="/logout.png" alt="" />
+                <p className="text-button">Fazer logout</p>
+              </ButtonLogout>
+            </div>
+          </div>
+        </section>
+      </Main>
+    </>
+  );
+};
