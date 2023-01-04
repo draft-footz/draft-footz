e45885052cee65ee69f8ffd1fa278751b2c30cb3
Arthur Vinícius <arthurnaocopia@gmail.com>
2023-01-04 15:46:23 -0300

    build: linked routes to respective pages

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,12 +1,16 @@
 import { Navigate, Route, Routes } from "react-router-dom";
+import { DashboardPage } from "../pages/Dashboard";
+import { LadingPage } from "../pages/LandingPage";
+import { LoginPage } from "../pages/Login";
+import { RegisterPage } from "../pages/Register";
 
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={} />
-      <Route path="/register" element={} />
-      <Route path="/login" element={} />
-      <Route path="/dashboard" element={} />
+      <Route path="/" element={<LadingPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/dashboard" element={<DashboardPage />} />
 
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
