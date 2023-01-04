70e0b4fa67e01fa12affa7882f622e0388293c86
Arthur Vinícius <arthurnaocopia@gmail.com>
2023-01-04 14:52:58 -0300

    feat: created social login components

---
+++ src/components/SocialLogin/Google/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { SocialLoginButton } from '../style'
+
+export const GoogleButton = () => {
+  return (
+    <SocialLoginButton>
+      <img src="/gmail.svg" alt=''/>
+      <span>Google</span>
+    </SocialLoginButton>
+  )
+}
