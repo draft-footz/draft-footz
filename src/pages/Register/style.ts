904da2fb5262aed6856f9fdba1d0e4ec84795931
Arthur Vinícius <arthurnaocopia@gmail.com>
2023-01-05 15:10:24 -0300

    fix/Reponsive register page

--- src/pages/Register/style.ts
+++ src/pages/Register/style.ts
@@ -15,6 +15,10 @@ export const StyledRegisterPage = styled.div`
 
   transition: 1s;
 
+  padding: 50px 0;
+
+  overflow-y: auto;
+
   h1 {
     font-weight: 500;
     font-size: 1.125rem;
@@ -30,29 +34,30 @@ export const StyledRegisterPage = styled.div`
 
     width: 650px;
 
-    padding: 45px 70px;
+    margin-top: 100px;
+
+    padding: 45px 50px;
     border-radius: 20px;
 
     background-color: var(--windowBackground);
   }
 
-  @media (max-width: 768px) {
+  @media (max-width: 1000px) {
     .window {
-      width: 75%;
-     padding: 30px 55px;
+      width: 80%;
+      padding: 30px 55px;
+      margin-top: 0;
     }
   }
   @media (max-width: 450px) {
-    
-
     .window {
-      width: 85%;
-     padding: 20px 35px;
+      width: 90%;
+      padding: 20px 35px;
 
-     gap: 10px
+      gap: 10px;
     }
   }
-  
+
   .social {
     display: flex;
     justify-content: center;
