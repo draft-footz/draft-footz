db5b9b48786e52ab2476cdae0530aba0027bbe14
victorglim <victor.guedes888@gmail.com>
2023-01-06 19:08:33 -0300

    feat: header implemented on homepage implemented with responsiveness

--- src/context/UsersContext.tsx
+++ src/context/UsersContext.tsx
@@ -2,38 +2,34 @@ import { useState, createContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { api } from "../services/api";
 import {
-  iDataLogin,
-  iDataNewUser,
-  iUserData,
-  iUserResponse,
-  iUsersContext,
-  iUsersProvider,
+    iDataLogin,
+    iDataNewUser,
+    iUserData,
+    iUserResponse,
+    iUsersContext,
+    iUsersProvider,
 } from "../types/UsersContextTypes";
-
 export const UserContext = createContext({} as iUsersContext);
-
 export const UsersProvider = ({ children }: iUsersProvider) => {
-  const navigate = useNavigate();
-  const [user, setUser] = useState({} as iUserData);
-  const [token, setToken] = useState("");
-  async function createNewUser(data: iDataNewUser) {
-    let newData = { ...data, myTeam: null };
-    try {
-      const requisition = await api.post("tournaments", newData);
-      console.log(requisition);
-      navigate("/login");
-    } catch (err) {
-      console.log(err);
+    const navigate = useNavigate();
+    const [user, setUser] = useState({} as iUserData);
+    const [token, setToken] = useState('');
+    async function createNewUser(data: iDataNewUser) {
+        let newData = { ...data, myTeam: null };
+        try {
+            const requisition = await api.post("tournaments", newData);
+            console.log(requisition);
+            navigate("/login");
+        } catch (err) {
+            console.log(err);
+        }
     }
-  }
-  
     const userLogin = async (data: iDataLogin) => {
         try {
             const requisition = await api.post("/login", data);
             console.log(requisition.data);
             localStorage.setItem("@AcessToken", requisition.data.accessToken);
             localStorage.setItem("@user", JSON.stringify(requisition.data.user.id));
-
             if(requisition.status === 200 ) {
                 setUser(requisition.data.user);
                 setToken(requisition.data.accessToken);
@@ -43,7 +39,6 @@ export const UsersProvider = ({ children }: iUsersProvider) => {
             console.log(error);
         }
     };
-
     async function updateUserTeam(teamId: number) {
         try {
             const requisition = await api.patch<iUserResponse>(
@@ -64,4 +59,18 @@ export const UsersProvider = ({ children }: iUsersProvider) => {
         } catch (err) {
             console.log(err);
         };
-
+    }
+    return (
+        <UserContext.Provider
+            value={{
+                createNewUser,
+                userLogin,
+                updateUserTeam,
+                user,
+                token
+            }}
+        >
+            {children}
+        </UserContext.Provider>
+    );
+};
--- src/pages/Homepage/style.ts
+++ src/pages/Homepage/style.ts
@@ -1,2 +1,11 @@
 import styled from "styled-components";
 
+export const FigureBackground = styled.div`
+
+  background-image: url('./gramado.png');
+  height: 100vh;
+  width: 100vw;
+  background-repeat: no-repeat;
+  background-size: cover;
+`
+
