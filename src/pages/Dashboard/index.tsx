3dcb3bac48258f80ab686dc9c8f303e767afd0da
Richard Jácomo <richard.jacomo23@gmail.com>
2023-01-06 18:31:51 -0300

    feat: removes all classes

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,16 @@
 import React, { useState } from "react";
 import { TournamentCreation } from "../../components/TournamentCreation";
-import { ButtonLogout, ButtonMenu } from "../../styles/Buttons/style";
+import { ButtonMenu } from "../../styles/Buttons/style";
 import { FigureBackgroundDashboard } from "../../styles/Figures/style";
-import { Main } from "./style";
+import {
+  ButtonLogout,
+  DivButtonLogout,
+  DivLogoAndButtons,
+  DivMenu,
+  FigureLogo,
+  Main,
+  SectionDashboard,
+} from "./style";
 
 export const DashboardPage = () => {
   const [value, setValue] = useState(0);
@@ -16,36 +24,30 @@ export const DashboardPage = () => {
         <img src="/bg-dashboard.png" alt="" />
       </FigureBackgroundDashboard>
       <Main>
-        <section className="section-dashboard">
-          <div className="div-menu">
-            <div className="div-logo-and-buttons">
-              <figure className="logo">
+        <SectionDashboard>
+          <DivMenu>
+            <DivLogoAndButtons>
+              <FigureLogo>
                 <img src="/logo.svg" alt="" />
-              </figure>
-              <ButtonMenu
-                onClick={() => handleClick(0)}
-                className="first-button-menu"
-              >
+              </FigureLogo>
+              <ButtonMenu onClick={() => handleClick(0)}>
                 Criar torneio
               </ButtonMenu>
-              <ButtonMenu
-                onClick={() => handleClick(1)}
-                className="secound-button-menu"
-              >
+              <ButtonMenu onClick={() => handleClick(1)}>
                 Meus torneios
               </ButtonMenu>
-              <ButtonMenu className="third-button-menu">Meu time</ButtonMenu>
-            </div>
-            <div className="div-button-logout">
-              <ButtonLogout className="button-logout">
-                <img className="icon-logout" src="/logout.png" alt="" />
-                <p className="text-button">Fazer logout</p>
+              <ButtonMenu onClick={() => handleClick(2)}>Meu time</ButtonMenu>
+            </DivLogoAndButtons>
+            <DivButtonLogout>
+              <ButtonLogout>
+                <img src="/logout.png" alt="" />
+                <p>Fazer logout</p>
               </ButtonLogout>
-            </div>
-          </div>
+            </DivButtonLogout>
+          </DivMenu>
           {value === 0 && <div />}
           {value === 1 && <TournamentCreation />}
-        </section>
+        </SectionDashboard>
       </Main>
     </>
   );
