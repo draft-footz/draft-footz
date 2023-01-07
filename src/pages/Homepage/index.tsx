09333ad577d2641d1729537d32d199df2d36b2b9
victorglim <victor.guedes888@gmail.com>
2023-01-07 17:34:14 -0300

    feat: added styling with main homepage features

--- src/pages/Homepage/index.tsx
+++ src/pages/Homepage/index.tsx
@@ -1,12 +1,21 @@
 import React from 'react'
+import FooterHome from '../../components/FooterHome'
 import Header from '../../components/Header'
-import { FigureBackground } from './style'
+import { DivHomeCenter, FigureBackground, LinkToRegister } from './style'
+
+
+
 
 const Homepage = () => {
   return (
     <>
       <FigureBackground>
         <Header />
+        <DivHomeCenter>
+          <h2>Crie seu campeonato agora</h2>
+          <LinkToRegister to='/register'>Novo campeonato</LinkToRegister>
+        </DivHomeCenter>
+        <FooterHome />     
       </FigureBackground>  
       
     </>
--- src/pages/Homepage/style.ts
+++ src/pages/Homepage/style.ts
@@ -1,11 +1,62 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 export const FigureBackground = styled.div`
-
-  background-image: url('./gramado.png');
+  background-image: url("./gramado.png");
   height: 100vh;
   width: 100vw;
   background-repeat: no-repeat;
   background-size: cover;
-`
+  position: relative;
+`;
 
+export const DivHomeCenter = styled.div`
+  font-family: "Poppins";
+  font-style: normal;
+  font-weight: 600;
+  font-size: 25px;
+  color: #ffffff;
+  display: flex;
+  flex-direction: column;
+  height: 70%;
+  justify-content: center;
+  align-items: center;
+  gap: 40px;
+
+  h2 {
+    font-family: "Poppins";
+    font-style: normal;
+    font-weight: 600;
+    font-size: 20px;
+    color: #ffffff;
+
+    @media (min-width: 425px) {
+        font-size: 22px;
+    }
+
+    @media (min-width: 768px) {
+        font-size: 25px;
+    }
+  }
+`;
+
+export const LinkToRegister = styled(Link)`
+  width: 215px;
+  height: 43px;
+  background: #00b148;
+  border-radius: 12px;
+  font-family: "Poppins";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 15px;
+  color: #ffffff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+
+
+  &:hover {
+    filter: brightness(0.9);
+  }
+`;
