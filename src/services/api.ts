ace6b5e582431e23cb43ed099314f5d85b4aa6a9
rgranatodutra <r.granatodutra@gmail.com>
2023-01-10 09:53:23 -0300

    fix: baseURL port

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const baseUrl = {
     render: 'https://draft-footz.onrender.com/',
     radmin: {
-        RenanPC: '26.38.126.227'
+        RenanPC: '26.38.126.227:3001'
     }
 };
 
