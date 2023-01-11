8a786f32c2af629501895ae323dcdb319897c6ec
Arthur Vinícius <arthurnaocopia@gmail.com>
2023-01-11 10:21:01 -0300

    fix: apply to tournament button fixed and fixed subscription counter too

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,11 +3,11 @@ import axios from "axios";
 const baseUrl = {
   render: "https://draft-footz.onrender.com/",
   radmin: {
-    RenanPC: "http://26.38.126.227:3001",
+    RenanPC: "http://localhost:3001",
   },
 };
 
 export const api = axios.create({
-  baseURL: baseUrl.render,
+  baseURL: baseUrl.radmin.RenanPC,
   timeout: 5000,
 });
