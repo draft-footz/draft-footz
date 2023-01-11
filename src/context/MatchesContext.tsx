48af649bd777ba92903c20c7a07ae39a6ed0b016
Arthur Vinícius <arthurnaocopia@gmail.com>
2023-01-11 15:48:03 -0300

    fix/removed console.log's

--- src/context/MatchesContext.tsx
+++ src/context/MatchesContext.tsx
@@ -1,90 +1,99 @@
 import { createContext, useContext, useState, useEffect } from "react";
 import { toast } from "react-toastify";
 import { api } from "../services/api";
-import { iMatchData, iMatchesContext, iMatchesProvider, iMatchScores, iMatchTeams } from "../types/MatchesContextTypes";
+import {
+  iMatchData,
+  iMatchesContext,
+  iMatchesProvider,
+  iMatchScores,
+  iMatchTeams,
+} from "../types/MatchesContextTypes";
 import { failMatch } from "../utils/toast";
 import { TournamentContext } from "./TournamentContext";
 import { UserContext } from "./UsersContext";
 
-
 export const MatchesContext = createContext({} as iMatchesContext);
 
-export const MatchesProvider = ({children}: iMatchesProvider) => {
+export const MatchesProvider = ({ children }: iMatchesProvider) => {
+  const { user, token } = useContext(UserContext);
+  const { readingTournament } = useContext(TournamentContext);
 
-    const { user, token } = useContext(UserContext);
-    const { readingTournament } = useContext(TournamentContext)
+  // Matches Data
+  const [tournamentMatches, setTournamentMatches] = useState(
+    [] as iMatchData[]
+  );
 
-    // Matches Data
-    const [tournamentMatches, setTournamentMatches] = useState([] as iMatchData[]);
+  useEffect(() => {
+    if (readingTournament) {
+      readThisTournamentMatches(readingTournament.id);
+    }
+  }, [readingTournament]);
 
-    useEffect(() => {
-        if(readingTournament) {
-            readThisTournamentMatches(readingTournament.id);
-        };
-        console.log(tournamentMatches)
-    }, [readingTournament]);
-
-    // Functions
-    async function createTournamentMatch(tournamentId: number, order: number) {
-        let data = {
-            userId: user.id,
-            tournamentId: tournamentId,
-            order: order,
-        };
-
-
-        try {
-            console.log(`Criando partida ${order}`)
-            api.post<iMatchData>('matches', data, {
-                headers: { authorization: `Bearer ${token}`}
-            });
-
-        } catch {
-            failMatch()
-        };
+  // Functions
+  async function createTournamentMatch(tournamentId: number, order: number) {
+    let data = {
+      userId: user.id,
+      tournamentId: tournamentId,
+      order: order,
     };
 
-    async function createMultipleTournamentMatches(tournamentId: number, numberOfMatches: number) {
-        let created = 0;
-        while ( numberOfMatches > created){
-            createTournamentMatch(tournamentId, created+1)
-            created++
-        };
-    };
+    try {
+      api.post<iMatchData>("matches", data, {
+        headers: { authorization: `Bearer ${token}` },
+      });
+    } catch {
+      failMatch();
+    }
+  }
 
-    async function deleteTournamentMatch(matchId: number) {
-        try {
-            api.delete(`matches/${matchId}`, {
-                headers : { authorization: `Bearer ${token}`},
-                data: { userId: user.id }
-            })
-        } catch {
-            console.log('deu ruim');
-        };
-    };
+  async function createMultipleTournamentMatches(
+    tournamentId: number,
+    numberOfMatches: number
+  ) {
+    let created = 0;
+    while (numberOfMatches > created) {
+      createTournamentMatch(tournamentId, created + 1);
+      created++;
+    }
+  }
 
-    async function deleteAllMatchesFromTournament(tournamentId: number) {
-        try {
-            await api.get<iMatchData[]>(`matches?&tournamentId=${tournamentId}`)
-            .then((response) => response.data.map(match => match.id))
-            .then((response) => response.forEach(id => {
-                deleteTournamentMatch(id);
-            }));
-        } catch {
-            console.log('Falha ao deletar partidas do torneio.');
-        };
-    };
+  async function deleteTournamentMatch(matchId: number) {
+    try {
+      api.delete(`matches/${matchId}`, {
+        headers: { authorization: `Bearer ${token}` },
+        data: { userId: user.id },
+      });
+    } catch (error) {
+      console.error("Erro na requisição");
+    }
+  }
 
-    async function readThisTournamentMatches(tournamentId: number) {
-        try {
-            await api.get<iMatchData[]>(`matches?&tournamentId=${tournamentId}`)
-            .then((response) => setTournamentMatches(response.data));
-        } catch {
-            console.log('Falha ao ler partidas do torenio.');
-        };
-    };
+  async function deleteAllMatchesFromTournament(tournamentId: number) {
+    try {
+      await api
+        .get<iMatchData[]>(`matches?&tournamentId=${tournamentId}`)
+        .then((response) => response.data.map((match) => match.id))
+        .then((response) =>
+          response.forEach((id) => {
+            deleteTournamentMatch(id);
+          })
+        );
+    } catch {
+      console.error("Falha ao deletar partidas do torneio.");
+    }
+  }
 
-    /* async function updateMatchTeams(matchId: number, data: iMatchTeams) {
+  async function readThisTournamentMatches(tournamentId: number) {
+    try {
+      await api
+        .get<iMatchData[]>(`matches?&tournamentId=${tournamentId}`)
+        .then((response) => setTournamentMatches(response.data));
+    } catch {
+      console.error("Falha ao ler partidas do torneio.");
+    }
+  }
+
+  /* async function updateMatchTeams(matchId: number, data: iMatchTeams) {
         try {
             api.patch(`matches/${matchId}`, {
                 headers: { authorization : `Bearer ${token}`},
@@ -98,7 +107,7 @@ export const MatchesProvider = ({children}: iMatchesProvider) => {
         };
     }; */
 
-    /* async function updateMatchScores(matchId: number,data: iMatchScores) {
+  /* async function updateMatchScores(matchId: number,data: iMatchScores) {
         try {
             api.patch(`matches/${matchId}`, {
                 headers: { authorization : `Bearer ${token}`},
@@ -112,14 +121,16 @@ export const MatchesProvider = ({children}: iMatchesProvider) => {
         };
     }; */
 
-    return (
-        <MatchesContext.Provider value={{
-            createMultipleTournamentMatches,
-            deleteAllMatchesFromTournament,
-            createTournamentMatch,
-            tournamentMatches
-        }} >
-            {children}
-        </MatchesContext.Provider>
-    );
-} ;
+  return (
+    <MatchesContext.Provider
+      value={{
+        createMultipleTournamentMatches,
+        deleteAllMatchesFromTournament,
+        createTournamentMatch,
+        tournamentMatches,
+      }}
+    >
+      {children}
+    </MatchesContext.Provider>
+  );
+};
