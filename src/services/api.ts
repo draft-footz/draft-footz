9beca7ddcd137ed8f9b608950a2e2ad4a4451316
Richard Jácomo <richard.jacomo23@gmail.com>
2023-01-11 16:43:24 -0300

    feat: creates mobile menu for home page

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,20 +1,20 @@
 import axios from "axios";
 
-const baseUrl = {
-  render: "https://draft-footz.onrender.com/",
-  radmin: {
-    RenanPC: "http://26.38.126.227:3001",
-  },
-  localhost: "http://localhost:3001",
-};
+// const baseUrl = {
+//   render: "https://draft-footz.onrender.com/",
+//   radmin: {
+//     RenanPC: "http://26.38.126.227:3001",
+//   },
+//   localhost: "http://localhost:3001",
+// };
 
 // Validando se está no PC do Renan
-// caso seja o PC do Renan a baseUrl deve ser localhost, 
+// caso seja o PC do Renan a baseUrl deve ser localhost,
 // caso contrário, IP do radmin
 
-const RenanPC = localStorage.getItem('@RenanPC');
+// const RenanPC = localStorage.getItem('@RenanPC');
 
 export const api = axios.create({
-  baseURL: RenanPC ? 'http://127.0.0.1:3001' : baseUrl.radmin.RenanPC,
-  timeout: 5000
+  baseURL: "https://draft-footz.onrender.com/",
+  timeout: 5000,
 });
