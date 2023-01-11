be40ee897395a1fc6e7c05fcd1e6861e38788b92
victorglim <victor.guedes888@gmail.com>
2023-01-11 17:51:34 -0300

    null

--- src/context/MatchesContext.tsx
+++ src/context/MatchesContext.tsx
@@ -1,126 +1,30 @@
 import { createContext, useContext, useState, useEffect } from "react";
 import { toast } from "react-toastify";
 import { api } from "../services/api";
-import {
-  iMatchData,
-  iMatchesContext,
-  iMatchesProvider,
-  iMatchScores,
-  iMatchTeams,
-} from "../types/MatchesContextTypes";
+import { iMatchData, iMatchesContext, iMatchesProvider, iMatchScores, iMatchTeams } from "../types/MatchesContextTypes";
 import { failMatch } from "../utils/toast";
 import { SubscriptionsContext } from "./SubscriptionsContext";
 import { TournamentContext } from "./TournamentContext";
 import { UserContext } from "./UsersContext";
-
 export const MatchesContext = createContext({} as iMatchesContext);
-export const MatchesProvider = ({ children }: iMatchesProvider) => {
-  const { user, token } = useContext(UserContext);
-  const { readingTournament } = useContext(TournamentContext);
-
-  // Matches Data
-  const [tournamentMatches, setTournamentMatches] = useState(
-    [] as iMatchData[]
-  );
-
-  useEffect(() => {
-    if (readingTournament) {
-      readThisTournamentMatches(readingTournament.id);
-    }
-  }, [readingTournament]);
-
-  // Functions
-  async function createTournamentMatch(tournamentId: number, order: number) {
-    let data = {
-      userId: user.id,
-      tournamentId: tournamentId,
-      order: order,
-    };
-
-    try {
-      api.post<iMatchData>("matches", data, {
-        headers: { authorization: `Bearer ${token}` },
-      });
-    } catch {
-      failMatch();
-    }
-  }
-
-  async function createMultipleTournamentMatches(
-    tournamentId: number,
-    numberOfMatches: number
-  ) {
-    let created = 0;
-    while (numberOfMatches > created) {
-      createTournamentMatch(tournamentId, created + 1);
-      created++;
-    }
-  }
-
-  async function deleteTournamentMatch(matchId: number) {
-    try {
-      api.delete(`matches/${matchId}`, {
-        headers: { authorization: `Bearer ${token}` },
-        data: { userId: user.id },
-      });
-    } catch (error) {
-      console.error("Erro na requisição");
-    }
-  }
-
-  async function deleteAllMatchesFromTournament(tournamentId: number) {
-    try {
-      await api
-        .get<iMatchData[]>(`matches?&tournamentId=${tournamentId}`)
-        .then((response) => response.data.map((match) => match.id))
-        .then((response) =>
-          response.forEach((id) => {
-            deleteTournamentMatch(id);
-          })
-        );
-    } catch {
-      console.error("Falha ao deletar partidas do torneio.");
-    }
-  }
-
-  async function readThisTournamentMatches(tournamentId: number) {
-    try {
-      await api
-        .get<iMatchData[]>(`matches?&tournamentId=${tournamentId}`)
-        .then((response) => setTournamentMatches(response.data));
-    } catch {
-      console.error("Falha ao ler partidas do torneio.");
-    }
-  }
-
-  /* async function updateMatchTeams(matchId: number, data: iMatchTeams) {
-
 export const MatchesProvider = ({children}: iMatchesProvider) => {
-
     const { user, token } = useContext(UserContext);
     const { readingTournament } = useContext(TournamentContext);
     const { updateSubscription } = useContext(SubscriptionsContext);
     const [ updater, setUpdater ] = useState(true);
-    
-
     useEffect(() => {
         api.defaults.headers.common.authorization = `Bearer ${token}`
     }, [token]);
-
     // Matches Data
     const [tournamentMatches, setTournamentMatches] = useState([] as iMatchData[]);
-
     useEffect(() => {
         if(readingTournament) {
             readThisTournamentMatches(readingTournament.id);
         };
     }, [readingTournament, updater]);
-
-        
     const semiFinalsI  = tournamentMatches.find(match => match.order === 5);
     const semiFinalsII = tournamentMatches.find(match => match.order === 6);
     const grandFinal   = tournamentMatches.find(match => match.order === 7);
-
     // Functions
     async function createTournamentMatch(tournamentId: number, order: number) {
         let data = {
@@ -128,19 +32,15 @@ export const MatchesProvider = ({children}: iMatchesProvider) => {
             tournamentId: tournamentId,
             order: order,
         };
-
-
         try {
             console.log(`Criando partida ${order}`)
             api.post<iMatchData>('matches', data, {
                 headers: { authorization: `Bearer ${token}`}
             });
-
         } catch {
             failMatch()
         };
     };
-
     async function createMultipleTournamentMatches(tournamentId: number, numberOfMatches: number) {
         let created = 0;
         while ( numberOfMatches > created){
@@ -148,7 +48,6 @@ export const MatchesProvider = ({children}: iMatchesProvider) => {
             created++
         };
     };
-
     async function deleteTournamentMatch(matchId: number) {
         try {
             api.delete(`matches/${matchId}`, {
@@ -159,7 +58,6 @@ export const MatchesProvider = ({children}: iMatchesProvider) => {
             console.log('deu ruim');
         };
     };
-
     async function deleteAllMatchesFromTournament(tournamentId: number) {
         try {
             await api.get<iMatchData[]>(`matches?&tournamentId=${tournamentId}`)
@@ -171,7 +69,6 @@ export const MatchesProvider = ({children}: iMatchesProvider) => {
             console.log('Falha ao deletar partidas do torneio.');
         };
     };
-
     async function readThisTournamentMatches(tournamentId: number) {
         try {
             await api.get<iMatchData[]>(`matches?&tournamentId=${tournamentId}`)
@@ -180,7 +77,6 @@ export const MatchesProvider = ({children}: iMatchesProvider) => {
             console.log('Falha ao ler partidas do torenio.');
         };
     };
-
     async function updateMatchTeams(matchId: number, data: iMatchTeams, subscriptions?: number[]) {
         try {
             api.patch(`matches/${matchId}`, data)
@@ -195,15 +91,13 @@ export const MatchesProvider = ({children}: iMatchesProvider) => {
             toast.error('Falha ao definir times da partida.');
         };
     };
-    
     async function updateMatchScores(matchId: number,data: iMatchScores, order: number) {
         try {
             api.patch(`matches/${matchId}`, data)
             .then(() => {
                 toast.success('Você definiu o palcar da partida com sucesso!');
                 setUpdater(!updater);
-
-                if ( order === 1) { 
+                if ( order === 1) {
                     if(semiFinalsI) { updateMatchTeams(semiFinalsI.id, {
                         teamA: {
                             id: data.winner.id,
@@ -211,8 +105,7 @@ export const MatchesProvider = ({children}: iMatchesProvider) => {
                         }
                     })};
                  };
-
-                 if ( order === 2) { 
+                 if ( order === 2) {
                     if(semiFinalsI) { updateMatchTeams(semiFinalsI.id, {
                         teamB: {
                             id: data.winner.id,
@@ -220,8 +113,7 @@ export const MatchesProvider = ({children}: iMatchesProvider) => {
                         }
                     })};
                  };
-
-                 if ( order === 3) { 
+                 if ( order === 3) {
                     if(semiFinalsII) { updateMatchTeams(semiFinalsII.id, {
                         teamA: {
                             id: data.winner.id,
@@ -229,8 +121,7 @@ export const MatchesProvider = ({children}: iMatchesProvider) => {
                         }
                     })};
                  };
-
-                 if ( order === 4) { 
+                 if ( order === 4) {
                     if(semiFinalsII) { updateMatchTeams(semiFinalsII.id, {
                         teamB: {
                             id: data.winner.id,
@@ -238,8 +129,7 @@ export const MatchesProvider = ({children}: iMatchesProvider) => {
                         }
                     })};
                  };
-
-                 if ( order === 5) { 
+                 if ( order === 5) {
                     if(grandFinal) { updateMatchTeams(grandFinal.id, {
                         teamA: {
                             id: data.winner.id,
@@ -247,8 +137,7 @@ export const MatchesProvider = ({children}: iMatchesProvider) => {
                         }
                     })};
                  };
-
-                 if ( order === 6) { 
+                 if ( order === 6) {
                     if(grandFinal) { updateMatchTeams(grandFinal.id, {
                         teamB: {
                             id: data.winner.id,
@@ -261,17 +150,16 @@ export const MatchesProvider = ({children}: iMatchesProvider) => {
             toast.error('Falha ao definir placar da partida.');
         };
     };
-
-  return (
-    <MatchesContext.Provider
-      value={{
-        createMultipleTournamentMatches,
-        deleteAllMatchesFromTournament,
-        createTournamentMatch,
-        tournamentMatches,
-      }}
-    >
-      {children}
-    </MatchesContext.Provider>
-  );
-};
+    return (
+        <MatchesContext.Provider value={{
+            createMultipleTournamentMatches,
+            deleteAllMatchesFromTournament,
+            createTournamentMatch,
+            tournamentMatches,
+            updateMatchTeams,
+            updateMatchScores
+        }} >
+            {children}
+        </MatchesContext.Provider>
+    );
+} ;
