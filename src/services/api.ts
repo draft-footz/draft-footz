73b343a04608dbfc9ed1aed9eb65e30053f7212f
Arthur Vinícius <arthurnaocopia@gmail.com>
2023-01-11 13:41:18 -0300

    fix: fixed apply to tournament

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,11 +3,12 @@ import axios from "axios";
 const baseUrl = {
   render: "https://draft-footz.onrender.com/",
   radmin: {
-    RenanPC: "http://localhost:3001",
+    RenanPC: "http://26.38.126.227:3001",
   },
+  localhost: "http://localhost:3001",
 };
 
 export const api = axios.create({
-  baseURL: baseUrl.radmin.RenanPC,
+  baseURL: baseUrl.localhost,
   timeout: 5000,
 });
