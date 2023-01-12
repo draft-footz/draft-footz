c6a24c7c820f0d3e649667cdfe7aa3f58cfd603f
rgranatodutra <r.granatodutra@gmail.com>
2023-01-12 01:18:59 -0300

    fix: tournamentView key props error

--- src/components/TournamentsView/index.tsx
+++ src/components/TournamentsView/index.tsx
@@ -5,12 +5,12 @@ import { TournamentCard } from "./TournamentCard";
 
 export const TournamentsView = () => {
     const { allTournaments } = useContext(TournamentContext);
-    
+
     return (
         <StyledTournamentsView>
             <h1> Torneios </h1>
             <ul>
-                {allTournaments.map(tournament => <TournamentCard tournament={tournament}/>)}
+                {allTournaments.map(tournament => <TournamentCard key={tournament.id} tournament={tournament}/>)}
             </ul>
         </StyledTournamentsView>
     );
